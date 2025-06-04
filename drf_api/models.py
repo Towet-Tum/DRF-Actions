7b44bbce643fd8c36dde649381a1c6a31e53b5cc
Towet-Tum <daimacvdl@gmail.com>
2025-06-04 15:25:08 +0300

    Fix: updated migration with correct formatting

--- drf_api/models.py
+++ drf_api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
