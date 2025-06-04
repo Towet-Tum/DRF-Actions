205e6e692ed82e6bc75dbf159e1dd8ac25b6e246
Towet-Tum <daimacvdl@gmail.com>
2025-06-04 14:48:13 +0300

    Add price field to Item model

--- drf_api/admin.py
+++ drf_api/admin.py
@@ -6,7 +6,7 @@ from .models import Item
 # Register your models here.
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "created_at")
+    list_display = ("name", "description", "price", "created_at")
     search_fields = ("name", "description")
     list_filter = ("created_at",)
     ordering = ("-created_at",)
